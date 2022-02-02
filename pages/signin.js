b481073b22ee16da48c154376a7ae45bb6788768
danghuytu12 <86486246+danghuytu12@users.noreply.github.com>
2022-02-02 19:15:40 +0700

    change textfiled login

--- pages/signin.js
+++ pages/signin.js
@@ -1,39 +1,48 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import {useState, useContext, useEffect} from 'react'
-import {DataContext} from '../store/GlobalState'
-import {postData} from '../utils/fetchData'
+import { useState, useContext, useEffect } from 'react'
+import { DataContext } from '../store/GlobalState'
+import { postData } from '../utils/fetchData'
 import Cookie from 'js-cookie'
 import { useRouter } from 'next/router'
+import Box from '@mui/material/Box';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 
 const Signin = () => {
   const initialState = { email: '', password: '' }
   const [userData, setUserData] = useState(initialState)
   const { email, password } = userData
 
-  const {state, dispatch} = useContext(DataContext)
+  const { state, dispatch } = useContext(DataContext)
   const { auth } = state
 
   const router = useRouter()
 
   const handleChangeInput = e => {
-    const {name, value} = e.target
-    setUserData({...userData, [name]:value})
+    const { name, value } = e.target
+    setUserData({ ...userData, [name]: value })
     dispatch({ type: 'NOTIFY', payload: {} })
   }
 
   const handleSubmit = async e => {
     e.preventDefault()
-    dispatch({ type: 'NOTIFY', payload: {loading: true} })
+    dispatch({ type: 'NOTIFY', payload: { loading: true } })
     const res = await postData('auth/login', userData)
-    
-    if(res.err) return dispatch({ type: 'NOTIFY', payload: {error: res.err} })
-    dispatch({ type: 'NOTIFY', payload: {success: res.msg} })
 
-    dispatch({ type: 'AUTH', payload: {
-      token: res.access_token,
-      user: res.user
-    }})
+    if (res.err) return dispatch({ type: 'NOTIFY', payload: { error: res.err } })
+    dispatch({ type: 'NOTIFY', payload: { success: res.msg } })
+
+    dispatch({
+      type: 'AUTH', payload: {
+        token: res.access_token,
+        user: res.user
+      }
+    })
 
     Cookie.set('refreshtoken', res.refresh_token, {
       path: 'api/auth/accessToken',
@@ -44,36 +53,49 @@ const Signin = () => {
   }
 
   useEffect(() => {
-    if(Object.keys(auth).length !== 0) router.push("/")
+    if (Object.keys(auth).length !== 0) router.push("/")
   }, [auth])
 
-    return(
-      <div>
-        <Head>
-          <title>Sign in Page</title>
-        </Head>
+  return (
+    <div>
+      <Head>
+        <title>Sign in Page</title>
+      </Head>
 
-        <form className="mx-auto my-4" style={{maxWidth: '500px'}} onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email address</label>
+      <form className="mx-auto my-4" style={{ maxWidth: '500px' }} onSubmit={handleSubmit}>
+        <div className="form-group">
+          {/* <label htmlFor="exampleInputEmail1">Email address</label>
             <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
             name="email" value={email} onChange={handleChangeInput} />
-            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-          </div>
-          <div className="form-group">
-            <label htmlFor="exampleInputPassword1">Password</label>
-            <input type="password" className="form-control" id="exampleInputPassword1"
+            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small> */}
+          <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+            <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+            <TextField
+              id="input-with-sx"
+              label="Email"
+              variant="standard"
+              type="email"
+              aria-describedby="emailHelp"
+              name="email"
+              value={email}
+              onChange={handleChangeInput}
+            />
+          </Box>
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <input type="password" className="form-control" id="exampleInputPassword1"
             name="password" value={password} onChange={handleChangeInput} />
-          </div>
-          
-          <button type="submit" className="btn btn-dark w-100">Login</button>
+        </div>
 
-          <p className="my-2">
-            You don't have an account? <Link href="/register"><a style={{color: 'crimson'}}>Register Now</a></Link>
-          </p>
-        </form>
-      </div>
-    )
-  }
-  
-  export default Signin
+        <button type="submit" className="btn btn-dark w-100">Login</button>
+
+        <p className="my-2">
+          You don't have an account? <Link href="/register"><a style={{ color: 'crimson' }}>Register Now</a></Link>
+        </p>
+      </form>
+    </div>
+  )
+}
+
+export default Signin
